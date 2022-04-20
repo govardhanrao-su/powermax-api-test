f518de2399c0e6e54e19ab7b2dfabb6bd537d545
Govardhan Rao Sunkishela <76477944+govardhanrao-su@users.noreply.github.com>
2022-04-20 23:46:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import powermax
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return """
+    Hello!
+    
+    Available endpoints are
+    - version
+    - arrays
+    - arrays/<array_id>
+    - headroom/<array_id>
+    """
+
+
+@app.route('/version')
+def show_ver():
+    return str(powermax.get_uni_ver())
+
+
+@app.route('/arrays')
+def show_arrays():
+    return str(powermax.list_arrays())
+
+
+@app.route('/arrays/<sid>')
+def show_array(sid):
+    return str(powermax.get_array(sid))
+
+
+@app.route('/headroom/<sid>')
+def show_capacity(sid):
+    return str(powermax.get_headroom(sid))
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
